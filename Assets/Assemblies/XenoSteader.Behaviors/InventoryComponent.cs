c9698c058c5c0ed3be9be7e13a3f0c44181e8753
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2019-07-22 21:16:38 -0400

    Added Behaviors assembly

---
+++ Assets/Assemblies/XenoSteader.Behaviors/InventoryComponent.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Assemblies.XenoSteader.Systems.Inventory;
+using UnityEngine;
+
+namespace Assets.Assemblies.XenoSteader.Behaviors
+{
+    public class InventoryComponent : MonoBehaviour
+    {
+        public InventorySystem Inventory { get; private set; }
+
+        public void Awake()
+        {
+            Inventory = new InventorySystem();
+        }
+    }
+}
