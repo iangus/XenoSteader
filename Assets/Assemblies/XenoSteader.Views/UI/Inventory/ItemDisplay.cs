8676b09b73b15286f19d28a5155f66590e17c3d6
Ian Guswiler <guswile2@msu.edu>
2019-07-26 20:22:07 -0400

    add ItemDisplay script

---
+++ Assets/Assemblies/XenoSteader.Views/UI/Inventory/ItemDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Assemblies.XenoSteader.Core.Objects;
+using TMPro;
+
+public class ItemDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI itemNameText;
+    public TextMeshProUGUI quantityText;
+    public Image itemImage;
+    public Item item;
+    
+    void Update()
+    {
+        itemNameText.text = item.name;
+        quantityText.text = "x" + item.quantity;
+        itemImage.sprite = item.sprite;
+    }
+}
